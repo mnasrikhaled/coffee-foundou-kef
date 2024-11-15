7a5e11c426c7cf0dc763e2fafe6263b5c964773b
mnasrikhaled <mnasrikhaled00@gmail.com>
2024-11-15 19:36:45 +0100

    Initial commit: Add Coffee Website files

---
+++ res.js
@@ -0,0 +1,5 @@
+document.getElementById("reservation-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Réservation soumise avec succès ! Nous vous contacterons pour confirmer les détails.");
+    // Vous pouvez ajouter ici le code pour envoyer la réservation à un serveur, si nécessaire.
+});
